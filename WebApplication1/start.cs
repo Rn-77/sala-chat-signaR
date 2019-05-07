36ebd5daf79bb907bd0e15ac0fc8166e18aaa796
agustin-giuliani <agustingiuliani45giuliani@gmail.com>
2019-05-07 17:17:42 -0300

    listo

---
+++ WebApplication1/start.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.SignalR;
+
+[assembly: OwinStartup(typeof(salaChat.start))]
+namespace salaChat
+{
+    public class start : Hub
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
